70b3d97b451b16145b7f64686037ca4e9c0f7c94
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-04-17 14:50:26 +0900

    Alert Action

--- UIAlert/ViewController.swift
+++ UIAlert/ViewController.swift
@@ -14,12 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    @IBAction func btnCamera(_ sender: Any) {
+        let myAlret = UIAlertController(title : "알림", message: "설정된 시간이 되었습니다.!", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler : {(myaction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.red
+            })
+        
+        let cancelAction = UIAlertAction(title: "취소", style : .cancel, handler : {(myaction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.white
+        })
+        
+        let testAction = UIAlertAction(title: "삭제", style: .destructive, handler: {(myaction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.green
+        })
+    myAlret.addAction(okAction)
+    myAlret.addAction(cancelAction)
+    myAlret.addAction(testAction)
+    
+    present(myAlret, animated: true, completion: nil)
+}
 }
 
